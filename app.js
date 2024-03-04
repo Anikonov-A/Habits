b1d27f44cce855bc77e0ab50c41ff92ffe83c5b7
Anton Anikonov <antonanikonov1@gmail.com>
2024-03-04 11:55:48 +0200

    added hmtl markup with styles

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
 "use strict";
+
+function submitForm() {
+  const input = document.querySelector(".input").value;
+  const formObject = {
+    text: input,
+  };
+  if (input) {
+    document.querySelector(".panel").textContent = input;
+    document.querySelector(".input").value = "";
+    document
+      .querySelector(".notification")
+      .classList.add("notification_active");
+    localStorage.setItem("text", JSON.stringify(formObject));
+  } else {
+    return;
+  }
+}
+function inputChange(e) {
+  if (e.code == "Enter" || e.code == "NumpadEnter") {
+    submitForm();
+  }
+}
