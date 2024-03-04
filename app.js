15b8e85e9439d2f4f170dc59c08543956f082cbd
Anton Anikonov <antonanikonov1@gmail.com>
2024-03-04 16:49:06 +0200

    Added simple logic for rerender menu components, removed unnecessary markup

--- app.js
+++ app.js
@@ -1,23 +1,57 @@
 "use strict";
 
-function submitForm() {
-  const input = document.querySelector(".input").value;
-  const formObject = {
-    text: input,
-  };
-  if (input) {
-    document.querySelector(".panel").textContent = input;
-    document.querySelector(".input").value = "";
-    document
-      .querySelector(".notification")
-      .classList.add("notification_active");
-    localStorage.setItem("text", JSON.stringify(formObject));
-  } else {
+let habits = [];
+const HABIT_KEY = "HABIT_KEY";
+//Page
+const page = {
+  menu: document.querySelector(".menu__list"),
+};
+// Utilits
+function loadData() {
+  const habitsString = localStorage.getItem(HABIT_KEY);
+  const habitsArr = JSON.parse(habitsString);
+  if (Array.isArray(habitsArr)) {
+    habits = habitsArr;
+  }
+}
+function saveData() {
+  localStorage.setItem(HABIT_KEY, JSON.stringify(habits));
+}
+
+// Render
+function rerenderMenu(activeHabit) {
+  if (!activeHabit) {
     return;
   }
-}
-function inputChange(e) {
-  if (e.code == "Enter" || e.code == "NumpadEnter") {
-    submitForm();
+  for (const habit of habits) {
+    const existed = document.querySelector(`[data-menu-id="${habit.id}"]`);
+    if (!existed) {
+      const element = document.createElement("button");
+      element.setAttribute("data-menu-id", habit.id);
+      element.classList.add("menu__item");
+      element.addEventListener("click", () => rerender(habit.id));
+      element.innerHTML = `<img src='./img/${habit.icon}.svg' alt='${habit.name}' />`;
+      console.log(element);
+      if (activeHabit.id === habit.id) {
+        element.classList.add("menu__item_active");
+      }
+      page.menu.appendChild(element);
+      continue;
+    }
+    if (activeHabit.id === habit.id) {
+      existed.classList.add("menu__item_active");
+    } else {
+      existed.classList.remove("menu__item_active");
+    }
   }
 }
+function rerender(activeHabitId) {
+  const activeHabit = habits.find((h) => h.id === activeHabitId);
+  rerenderMenu(activeHabit);
+}
+
+//Init
+(() => {
+  loadData();
+  rerender(habits[0].id);
+})();
