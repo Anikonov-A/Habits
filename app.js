6d9ded8586aaf78bef0b0bc654a9cae94e4d8ff7
Anton Anikonov <antonanikonov1@gmail.com>
2024-03-05 12:25:58 +0200

    Added logick for render header component with progress bar, and days component with comments.

--- app.js
+++ app.js
@@ -5,6 +5,16 @@ const HABIT_KEY = "HABIT_KEY";
 //Page
 const page = {
   menu: document.querySelector(".menu__list"),
+  header: {
+    title: document.querySelector(".title"),
+    progressPercent: document.querySelector(".progress__percent"),
+    progressCoverBar: document.querySelector(".progress__cover-bar"),
+  },
+  content: {
+    daysWrapper: document.querySelector(".main__wrapper"),
+    day: document.querySelector(".habit__day"),
+    comment: document.querySelector(".habit__comment"),
+  },
 };
 // Utilits
 function loadData() {
@@ -20,9 +30,6 @@ function saveData() {
 
 // Render
 function rerenderMenu(activeHabit) {
-  if (!activeHabit) {
-    return;
-  }
   for (const habit of habits) {
     const existed = document.querySelector(`[data-menu-id="${habit.id}"]`);
     if (!existed) {
@@ -31,7 +38,6 @@ function rerenderMenu(activeHabit) {
       element.classList.add("menu__item");
       element.addEventListener("click", () => rerender(habit.id));
       element.innerHTML = `<img src='./img/${habit.icon}.svg' alt='${habit.name}' />`;
-      console.log(element);
       if (activeHabit.id === habit.id) {
         element.classList.add("menu__item_active");
       }
@@ -45,9 +51,44 @@ function rerenderMenu(activeHabit) {
     }
   }
 }
+function renderHead(activeHabit) {
+  page.header.title.innerText = activeHabit.name;
+  const progress =
+    activeHabit.days.lenght / activeHabit.target > 1
+      ? 100
+      : (activeHabit.days.length / activeHabit.target) * 100;
+
+  page.header.progressPercent.innerText = `${progress.toFixed(0)}%`;
+  page.header.progressCoverBar.style.width = `${progress}%`;
+}
+function rerenderContent(activeHabit) {
+  page.content.daysWrapper.innerHTML = "";
+  for (const i in activeHabit.days) {
+    const element = document.createElement("div");
+    element.classList.add("habit");
+    element.innerHTML = `<div class="habit__day">Day ${Number(i) + 1}</div>
+						<div class="habit__comment">${activeHabit.days[i].comment}</div>
+						<button class="habit__delete">
+							<img src="img/delete.svg" alt="delete day ${i + 1}">
+						</button>`;
+    page.content.daysWrapper.appendChild(element);
+  }
+  page.content.day.innerHTML = `Day ${activeHabit.days.length + 1}`;
+}
+
 function rerender(activeHabitId) {
   const activeHabit = habits.find((h) => h.id === activeHabitId);
+  if (!activeHabit) {
+    return;
+  }
   rerenderMenu(activeHabit);
+  renderHead(activeHabit);
+  rerenderContent(activeHabit);
+}
+//Work with days 
+function addDays(event) {
+  event.preventDefault();
+  
 }
 
 //Init
